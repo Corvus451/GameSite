bbc6c52efd8fbba7a1b52d5ebad9089ee0a22cf7
Corvus451 <msmark.451@gmail.com>
2025-11-17 10:50:17 +0100

    Terraform set backend to remote s3

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "gamesiteproject-state-bucket"
+    key = "terraform.tfstate"
+    region = "eu-central-1"
+    encrypt = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
